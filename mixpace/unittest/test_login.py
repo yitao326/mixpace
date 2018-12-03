6f66a44a7ac313e8b982e9ab464d4995cffca296
yitao <yitao326@163.com>
2018-12-03 18:31:55 +0800

    笔记本提交

--- mixpace/unittest/test_login.py
+++ mixpace/unittest/test_login.py
@@ -2,6 +2,9 @@ from mixpace.unittest.myunit import StartEnd
 from mixpace.page_object.loginView import LoginView
 import unittest
 import logging
+import warnings
+
+warnings.filterwarnings('ignore')
 
 class TestLogin(StartEnd):
     def test_login_15300752801(self):
@@ -9,15 +12,15 @@ class TestLogin(StartEnd):
         l = LoginView(self.driver)
         l.login_action('15300752801', '111111')
 
-    def test_login_15300752802(self):
-        logging.info('===test_login_15300752802===')
-        l = LoginView(self.driver)
-        l.login_action('15300752802', '111111')
-
-    def test_login_error(self):
-        logging.info('===test_login_error===')
-        l = LoginView(self.driver)
-        l.login_action('18000000000', '222222')
+    # def test_login_15300752802(self):
+    #     logging.info('===test_login_15300752802===')
+    #     l = LoginView(self.driver)
+    #     l.login_action('15300752802', '111111')
+    #
+    # def test_login_error(self):
+    #     logging.info('===test_login_error===')
+    #     l = LoginView(self.driver)
+    #     l.login_action('18000000000', '222222')
 
 if __name__ == '__main__':
     unittest.main()
